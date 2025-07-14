fd255179c1af5cc5d288052e6aeb62d81692a665
Danny van Heumen <danny@dannyvanheumen.nl>
2025-07-15 01:18:55 +0200

    UI: document import case-handling, clean up logging

--- cmd/doclib/main.go
+++ cmd/doclib/main.go
@@ -10,31 +10,9 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"github.com/cobratbq/doclib/internal/repo"
 	"github.com/cobratbq/goutils/std/builtin"
-	"github.com/cobratbq/goutils/std/errors"
 	io_ "github.com/cobratbq/goutils/std/io"
 	"github.com/cobratbq/goutils/std/log"
 )
-
-func queryDocumentName(parent fyne.Window) (string, error) {
-	inputName := widget.NewEntry()
-	// FIXME perform proper validation of file name
-	inputName.Validator = func(s string) error {
-		if len(s) > 0 {
-			return nil
-		}
-		return errors.ErrIllegal
-	}
-	resultChan := make(chan bool, 0)
-	dialog.ShowForm("Object name", "Store", "Cancel", []*widget.FormItem{widget.NewFormItem("Name:", inputName)},
-		func(proceed bool) {
-			log.Traceln("Dialog called with result:", proceed)
-			resultChan <- proceed
-		}, parent)
-	if !<-resultChan {
-		return "", errors.Context(errors.ErrFailure, "Dialog cancelled")
-	}
-	return inputName.Text, nil
-}
 
 func constructUI(parent fyne.Window, docrepo *repo.Repo) *fyne.Container {
 	lblStatus := widget.NewLabel("")
@@ -78,28 +56,28 @@ func constructUI(parent fyne.Window, docrepo *repo.Repo) *fyne.Container {
 	btnAcquire := widget.NewButton("Acquire", func() {
 		openDialog := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 			if err != nil {
-				log.Traceln("Failed to open file, possibly dialog cancelled: ", err.Error())
-				lblStatus.SetText("Dialog aborted. No file was selected.")
+				log.Warnln("Error opening file-dialog: ", err.Error())
+				lblStatus.SetText("File-dialog failed.")
+				return
+			}
+			if reader == nil {
+				log.Traceln("Document import was cancelled by user.")
+				lblStatus.SetText("")
 				return
 			}
 			defer io_.CloseLogged(reader, "Failed to gracefully close file.")
-			log.Warnln("File-dialog:", reader.URI(), err)
-			// FIXME make CopyFrom copy to temporary location, then wait for file to complete acquisition
 			if _, err = docrepo.Acquire(reader, reader.URI().Name()); err == nil {
 				log.Traceln("File-dialog successfully completed.")
 			} else {
 				log.Traceln("Failed to copy document into repository:", err.Error())
 				lblStatus.SetText("Failed to import document into repository: " + err.Error())
 			}
+			listObjects.Refresh()
+			log.Traceln("Document import completed.")
 		}, parent)
 		// FIXME fine-tune open-file dialog.
-		//openDialog.SetTitleText()
+		openDialog.SetTitleText("Document for import")
 		openDialog.Show()
-		listObjects.Refresh()
-
-		// FIXME WIP, continue with multi-step document acquisition ...
-
-		log.Traceln("Document acquired.")
 	})
 	btnCheck := widget.NewButton("Check", func() {
 		// FIXME no error handling
