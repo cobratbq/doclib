702091cf2856bda80423b7d16c98c7fce409cee9
Danny van Heumen <danny@dannyvanheumen.nl>
2025-07-27 22:20:11 +0200

    lib: switch to utils and closures

--- internal/repo/utils.go
+++ internal/repo/utils.go
@@ -5,19 +5,25 @@ import (
 	"strings"
 
 	"github.com/cobratbq/goutils/std/builtin"
+	slices_ "github.com/cobratbq/goutils/std/builtin/slices"
 )
 
-func IndexObjectByID(collection []RepoObj, id string) int {
-	for i, obj := range collection {
-		if obj.Id == id {
-			return i
-		}
+func objIdEquals(id string) func(o RepoObj) bool {
+	return func(o RepoObj) bool {
+		return o.Id == id
 	}
-	return -1
+}
+
+func IndexObjectByID(collection []RepoObj, id string) int {
+	return slices_.IndexFunc(collection, objIdEquals(id))
+}
+
+func objNameCompare(a, b RepoObj) int {
+	return strings.Compare(a.Name, b.Name)
 }
 
 func ExtractRepoObjectsSorted(docrepo *Repo) []RepoObj {
 	objects := builtin.Expect(docrepo.List())
-	slices.SortFunc(objects, func(a, b RepoObj) int { return strings.Compare(a.Name, b.Name) })
+	slices.SortFunc(objects, objNameCompare)
 	return objects
 }
